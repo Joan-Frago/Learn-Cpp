2bbabbc3d8d7725e048a09abc3d853a734708a00
Joan-Frago <150782224+Joan-Frago@users.noreply.github.com>
2024-05-30 10:47:09 +0200

    test

---
+++ test.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+# include <string>
+using namespace std;
+
+
+int main() {
+    string Name;
+    cout << "Your name is... ";
+    cin >> Name;
+    cout << "Your name is " + Name + "!";
+}
