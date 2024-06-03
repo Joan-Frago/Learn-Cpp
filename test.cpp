2280da1fcfa6a0016b5af4719d696e90daebad50
Joan-Frago <150782224+Joan-Frago@users.noreply.github.com>
2024-06-03 12:23:00 +0200

    testing

--- test.cpp
+++ test.cpp
@@ -1,11 +1,14 @@
 # include <iostream>
-# include <string>
 using namespace std;
 
 
 int main() {
-    string Name;
-    cout << "Your name is... ";
-    cin >> Name;
-    cout << "Your name is " + Name + "!";
+    int num;
+    cout << "Put a number: ";
+    cin >> num;
+
+    for (int i; i != num + 1; i++) {
+        cout << i << endl;
+    }
+    return 0;
 }
