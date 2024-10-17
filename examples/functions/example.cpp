48c3036fdc7f2aa4018d045ab8202e618012e5b3
Joan Frago <jfragojimenez@gmail.com>
2024-10-17 12:05:21 -0400

    examples of functions

---
+++ examples/functions/example.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void printNames(string fname) {
+    cout << fname << " Refsnes\n";
+}
+
+int main() {
+    printNames("Liam");
+    printNames("Jenny");
+    printNames("Anja");
+    return 0;
+}
